a590325b26d8b072d591719f3fdbcc98e1463da3
Vicra <honvramirez@gmail.com>
2023-02-22 18:42:46 -0600

    prints, depth, search

--- semana6/binaryTree/Tree.h
+++ semana6/binaryTree/Tree.h
@@ -13,8 +13,18 @@ public:
 
     TreeNode* insert(TreeNode *currentRoot, TreeNode* newNode);
 
+    TreeNode* mostLeftSon(TreeNode* root);
+    TreeNode* mostRightSon(TreeNode* root);
+
+    // print algorithms
     void preorder(TreeNode* root);
+    void inorder(TreeNode* root);
+    void postorden(TreeNode* root);
+
     void print(string prefix, TreeNode* root, bool isLeft);
+    // search
+    TreeNode* search(TreeNode* root, int key);
+    int depth(TreeNode* root, TreeNode* node, int interations);
 };
 
 Tree::Tree(/* args */)
@@ -23,6 +33,34 @@ Tree::Tree(/* args */)
 
 Tree::~Tree()
 {
+}
+
+// search(5)
+inline TreeNode *Tree::search(TreeNode* currentRoot, int key){
+    if(currentRoot == nullptr) {
+        return currentRoot;
+    }
+    else if (currentRoot->getValue() == key){
+        return currentRoot;
+    }
+    else if (key < currentRoot->getValue()){
+        return search(currentRoot->left, key);
+    }
+    else if (key > currentRoot->getValue()) {
+        return search(currentRoot->right, key);
+    }
+}
+
+inline int Tree::depth(TreeNode *currentRoot, TreeNode *node, int iterations)
+{
+    if(currentRoot == node)
+        return iterations;
+    if(node->getValue() < currentRoot->getValue()){
+        return depth(currentRoot->left, node, iterations + 1);
+    }
+    if(node->getValue() > currentRoot->getValue()){
+        return depth(currentRoot->right, node, iterations + 1);
+    }
 }
 
 inline TreeNode *Tree::insert(TreeNode *currentRoot, TreeNode *newNode)
@@ -42,12 +80,56 @@ inline TreeNode *Tree::insert(TreeNode *currentRoot, TreeNode *newNode)
     return currentRoot;
 }
 
+inline TreeNode *Tree::mostLeftSon(TreeNode *currentRoot)
+{
+    if(currentRoot == nullptr) 
+        return currentRoot;
+
+    if(currentRoot->left == nullptr){
+        return currentRoot;
+    }
+    else {
+        return mostLeftSon(currentRoot->left);
+    }
+}
+
+inline TreeNode *Tree::mostRightSon(TreeNode *currentRoot)
+{
+    if(currentRoot == nullptr) 
+        return currentRoot;
+
+    if(currentRoot->right == nullptr){
+        return currentRoot;
+    }
+    else {
+        return mostRightSon(currentRoot->right);
+    }
+}
+
 inline void Tree::preorder(TreeNode *currentRoot)
 {
     if(currentRoot != nullptr) {
         cout << currentRoot->getValue() << " ";
         preorder(currentRoot->left);
         preorder(currentRoot->right);
+    }
+}
+
+inline void Tree::inorder(TreeNode *currentRoot)
+{
+    if(currentRoot != nullptr) {
+        inorder(currentRoot->left);
+        cout << currentRoot->getValue() << " ";
+        inorder(currentRoot->right);
+    }
+}
+
+inline void Tree::postorden(TreeNode *currentRoot)
+{
+    if(currentRoot != nullptr) {
+        postorden(currentRoot->left);
+        postorden(currentRoot->right);
+        cout << currentRoot->getValue() << " ";
     }
 }
 
@@ -65,7 +147,8 @@ inline void Tree::print(string prefix, TreeNode *currentRoot, bool isLeft)
         // enter the next tree level - left and right branch
         print( prefix + (isLeft ? "│   " : "    "), currentRoot->left, true);
         print( prefix + (isLeft ? "│   " : "    "), currentRoot->right, false);
-    } else {
+    }
+    else {
         cout << prefix;
         cout << (isLeft ? "├─" : "└─" );
         cout << "nil" << endl;
--- semana6/binaryTree/main.cpp
+++ semana6/binaryTree/main.cpp
@@ -3,12 +3,42 @@
 int main(int argc, char const *argv[])
 {
     Tree* t = new Tree();
-    t->root = t->insert(t->root, new TreeNode(2));
-    t->root = t->insert(t->root, new TreeNode(3));
-    t->root = t->insert(t->root, new TreeNode(1));
-    t->root = t->insert(t->root, new TreeNode(4));
+    t->root = t->insert(t->root, new TreeNode(5));
+    t->insert(t->root, new TreeNode(4));
+    t->insert(t->root, new TreeNode(9));
+    t->insert(t->root, new TreeNode(1));
+    t->insert(t->root, new TreeNode(12));
+    t->insert(t->root, new TreeNode(7));
+    t->insert(t->root, new TreeNode(11));
 
-    // t->preorder(t->root);
+    TreeNode* mostLeft = t->mostLeftSon(t->root);
+    cout << "Hijo mas izquierdo: " << mostLeft->getValue() << "\n";
+
+    TreeNode* mostRight = t->mostRightSon(t->root);
+    cout << "Hijo mas derecho: " << mostRight->getValue() << "\n";
+
+    cout << "Preorder: " << endl;
+    t->preorder(t->root);
+    cout << endl;
+
+    cout << "Inorder ASC: " << endl;
+    t->inorder(t->root);
+    cout << endl; 
+
+    cout << "Postorden ASC: " << endl;
+    t->postorden(t->root);
+    cout << endl << endl; 
+
+    cout << "Searching 2..." << endl;
+    TreeNode * searchedNode = t->search(t->root, 1);
+    if(searchedNode != nullptr) {
+        cout << "Yes, exists" << endl;
+        cout << "value: " <<searchedNode->getValue() << endl;
+    } else {
+        cout << "No exists, what a shame" << endl;
+    }
+
+    cout << "Depth 1:" << t->depth(t->root, searchedNode, 0) << endl;
     t->print("", t->root, false);
     return 0;
 }
