7a7ed5a590dd9bb9933e9c9618efdc6670dfe881
Vicra <honvramirez@gmail.com>
2023-03-10 18:36:39 -0600

    Delete Node main

--- semana8/AVL/main.cpp
+++ semana8/AVL/main.cpp
@@ -2,17 +2,19 @@
 
 int main()
 {
-	AVLTree* t = new AVLTree();
+  AVLTree* t = new AVLTree();
   t->root = t->insert(t->root, 10);
   t->root = t->insert(t->root, 20);
   t->root = t->insert(t->root, 30);
   t->root = t->insert(t->root, 40);
   t->root = t->insert(t->root, 50);
   t->root = t->insert(t->root, 60);
+
+  t->print(t->root);
+
+  t->root = t->deleteNode(t->root, 40);
 	
-  // t->inorder(t->root);
-  // t->print("", t->root, false);
-  t->printTree(t->root, nullptr, false);
+  t->print(t->root);
 
 	return 0;
 }
